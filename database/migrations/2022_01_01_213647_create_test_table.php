a861bc57a446af27be5393feee416eab035e1245
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2022-01-01 20:54:38 -0300

    new branch photographs

--- database/migrations/2022_01_01_213647_create_test_table.php
+++ database/migrations/2022_01_01_213647_create_test_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTweetsTable extends Migration
+class CreateTestTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateTweetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tweets', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();            
-            $table->string('tweet');
+        Schema::create('test', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateTweetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tweets');
+        Schema::dropIfExists('test');
     }
 }
